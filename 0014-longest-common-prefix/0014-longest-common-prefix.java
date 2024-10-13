0bff88e5889d0afb14e6fdedfffb25465f0e93fb
Asmaa Mestaoui <asmaamestaoui11@gmail.com>
2024-10-13 15:09:23 +0200

    Time: 2 ms (24.12%), Space: 41.4 MB (54.17%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,31 +1,33 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        int min= min(strs);
-        String result="";
-        int s= strs[min].length();
-        if( s>-1){
-            for (int i=0; i<strs.length; i++){
-                for(int j=0; j<=s;j++){
-                    if(strs[min].charAt(j)!= strs[i].charAt(j)){
-                        s=j-1;
-                        break;
-                    }
+        if (strs == null || strs.length == 0) return "";
 
+        int minIndex = min(strs);
+        int minLength = strs[minIndex].length();
+        String result = "";
+
+        for (int i = 0; i < strs.length; i++) {
+            for (int j = 0; j < minLength; j++) {
+                if (strs[minIndex].charAt(j) != strs[i].charAt(j)) {
+                    minLength = j; 
+                    break; 
                 }
             }
         }
-        if (s>=0){
-            result=strs[0].substring(0,s); 
+
+        if (minLength > 0) {
+            result = strs[minIndex].substring(0, minLength);
         }
         return result;
     }
-    public int min(String[] strs){
-        int min=0;
-        for (int i=1; i<strs.length; i++) {
-            if (strs[i].length() < strs[min].length()){
-                min=i;
+
+    public int min(String[] strs) {
+        int minIndex = 0;
+        for (int i = 1; i < strs.length; i++) {
+            if (strs[i].length() < strs[minIndex].length()) {
+                minIndex = i;
             }
         }
-        return min;
+        return minIndex;
     }
 }
