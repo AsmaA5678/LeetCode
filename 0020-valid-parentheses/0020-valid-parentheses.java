a273b63790ac5ac079b86c822eb6eebc18b0a0f3
Asmaa Mestaoui <asmaamestaoui11@gmail.com>
2024-10-14 11:16:29 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+       char[]  ch=s.toCharArray();
+       List<Character> par = new ArrayList<>(); 
+        for (int i=0; i<ch.length;i++){
+            char c = ch[i];
+            if(c=='[' || c=='{'|| c=='(' ){
+                par.add(c);
+            }else{
+                if(par.isEmpty()){
+                    return false;
+                }
+                char last = par.get(par.size()-1);
+                if((c==']' && last=='[')||(c=='}' && last=='{' )|| (c==')' && last=='(' ) ){
+                par.remove(par.size()-1);
+            }
+            }
+           
+           
+        }
+        return par.isEmpty();
+
+
+    }
+}
