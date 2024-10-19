6c42337a5f67fc118e62e04aaebfbe532661cc23
Asmaa Mestaoui <asmaamestaoui11@gmail.com>
2024-10-19 21:52:10 +0100

    Time: 3 ms (5.63%), Space: 41.8 MB (49.58%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,19 +1,21 @@
+import java.math.BigInteger;
+
 class Solution {
     public int[] plusOne(int[] digits) {
-        String str = "";
+        StringBuilder str = new StringBuilder();
         for (int i = 0; i < digits.length; i++) {
-            str += digits[i];
+            str.append(digits[i]);
         }
-        int number = Integer.parseInt(str) + 1;
         
-        String newStr = Integer.toString(number);
+        BigInteger number = new BigInteger(str.toString());
+        number = number.add(BigInteger.ONE);
+        
+        String newStr = number.toString();
         
         int[] result = new int[newStr.length()];
-        
         for (int i = 0; i < newStr.length(); i++) {
             result[i] = newStr.charAt(i) - '0'; 
         }
-        
         return result;
     }
 }
