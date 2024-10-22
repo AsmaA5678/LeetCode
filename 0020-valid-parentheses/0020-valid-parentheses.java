fae9e670cf559e02b070fad8be619bc92237061b
Asmaa Mestaoui <asmaamestaoui11@gmail.com>
2024-10-23 00:43:39 +0200

    Time: 2 ms (60.77%), Space: 41.8 MB (19.92%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -4,14 +4,14 @@ class Solution {
        List<Character> par = new ArrayList<>(); 
         for (int i=0; i<ch.length;i++){
             char c = ch[i];
-            if(c=='[' || c=='{'|| c=='(' ){
+            if(c=='[' || c=='{' || c=='('){
                 par.add(c);
             }else{
                 if(par.isEmpty()){
                     return false;
                 }
                 char last = par.get(par.size()-1);
-                if((c==']' && last=='[')||(c=='}' && last=='{' )|| (c==')' && last=='(' ) ){
+                if ((c==']' && last=='[') || (c=='}' && last=='{' ) || (c==')' && last=='(' ) ){
                 par.remove(par.size()-1);
                 }
                 else {
