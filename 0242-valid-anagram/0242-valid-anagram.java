43c0ede40d894fd7e4693170d598d66ecff14bc0
Asmaa Mestaoui <asmaamestaoui11@gmail.com>
2024-10-31 21:34:22 +0100

    Time: 13 ms (33.45%), Space: 44.7 MB (19.92%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,10 +3,8 @@ class Solution {
         if (s.length() != t.length()) {
             return false;
         }
-
         HashMap<Character, Integer> sCount = count(s);
         HashMap<Character, Integer> tCount = count(t);
-
         return sCount.equals(tCount);
     }
     public static HashMap<Character, Integer> count(String string) {
