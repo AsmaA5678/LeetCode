e8460f29d02dcf690dbec688956d5928f4b72bcd
Asmaa Mestaoui <asmaamestaoui11@gmail.com>
2024-11-08 21:56:47 +0100

    Time: 17 ms (38.2%), Space: 42.5 MB (39.21%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,19 +1,15 @@
+import java.util.HashMap;
+
 class Solution {
     public boolean isIsomorphic(String s, String t) {
         if (s.length() != t.length()) {
             return false;
         }
-        HashMap<Character, Integer> sHash = count(s);
-        HashMap<Character, Integer> tHash = count(t);
         HashMap<Character, Character> mapSToT = new HashMap<>();
         HashMap<Character, Character> mapTToS = new HashMap<>();
-
         for (int i = 0; i < s.length(); i++) {
             char charS = s.charAt(i);
             char charT = t.charAt(i);
-            if (!sHash.get(charS).equals(tHash.get(charT))) {
-                return false;
-            }
             if (mapSToT.containsKey(charS)) {
                 if (mapSToT.get(charS) != charT) {
                     return false; 
@@ -23,21 +19,12 @@ class Solution {
             }
             if (mapTToS.containsKey(charT)) {
                 if (mapTToS.get(charT) != charS) {
-                    return false;
+                    return false; 
                 }
             } else {
                 mapTToS.put(charT, charS);
             }
         }
-
         return true;
-    }
-
-    public static HashMap<Character, Integer> count(String string) {
-        HashMap<Character, Integer> charCount = new HashMap<>();
-        for (char c : string.toCharArray()) {
-            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
-        }
-        return charCount;
     }
 }
