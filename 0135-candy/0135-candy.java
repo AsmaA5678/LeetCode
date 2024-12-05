8048ed6e388710bf6810ad81e5ad64fd4a9a9717
Asmaa Mestaoui <asmaamestaoui11@gmail.com>
2024-12-05 15:40:21 +0100

    Time: 2 ms (99.62%), Space: 45.3 MB (84.43%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,21 +1,24 @@
 class Solution {
     public int candy(int[] ratings) {
-        int n=ratings.length;
-        int[] candies=new int[n];
-        Arrays.fill(candies,1);
-        for(int i=0;i<n-1;i++){
-            if(ratings[i]>ratings[i+1]){
-                candies[i]+=1;
+        int n = ratings.length;
+        int[] candies = new int[n];
+        Arrays.fill(candies, 1); 
+
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
             }
         }
-        for(int i=n-1;i>0;i--){
-             if(ratings[i]>ratings[i-1]){
-                candies[i]=Math.max(candies[i-1], candies[i ] + 1);
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
             }
         }
-        int answer=0;
-        for(int i=0;i<n;i++){
-            answer+=candies[i];
+
+        int answer = 0;
+        for (int candy : candies) {
+            answer += candy;
         }
         return answer;
     }
