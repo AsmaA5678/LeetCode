aacbf0574c17788cb9259a779f16e82c5b7b61a6
Asmaa Mestaoui <asmaamestaoui11@gmail.com>
2024-12-11 11:29:44 +0100

    Time: 4 ms (52.27%), Space: 43.4 MB (52.55%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,15 +1,14 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String text = cleanString(s);
-        String lowerText = text.toLowerCase();
-        for (int i = 0; i < lowerText.length() / 2; i++) {
-            if (lowerText.charAt(i) != lowerText.charAt(lowerText.length() - 1 - i)) {
-                return false;
+        String text= cleanString(s);
+        String lowerText= text.toLowerCase();
+        for(int i=0; i< lowerText.length()/2;i++){
+            if(lowerText.charAt(i)!=lowerText.charAt(lowerText.length()-1-i)){
+            return false;
             }
         }
         return true;
     }
-
     public static String cleanString(String s) {
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
