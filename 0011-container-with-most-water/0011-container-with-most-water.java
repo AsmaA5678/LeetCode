edd4660f6c009201b72701a48797ec3cc445c8bb
Asmaa Mestaoui <asmaamestaoui11@gmail.com>
2024-12-11 10:21:10 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -5,14 +5,13 @@ class Solution {
         int max= 0;
 
     while (left < right) {
-
         int area = Math.min(height[left], height[right]) * (right - left);
         max = Math.max(max, area);
 
             if(height[left]>height[right]){
                 right--;
             }
-            else {
+            if(height[left]<height[right]){
                 left++;
             }
             
