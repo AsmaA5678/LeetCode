346a6587f816e1355f63e6bb05487675a12aa912
Asmaa Mestaoui <asmaamestaoui11@gmail.com>
2025-01-28 16:04:37 +0100

    Time: 1 ms (71.85%), Space: 41.8 MB (30.25%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,7 +1,7 @@
 class Solution {
     public int hIndex(int[] citations) {
         int n = citations.length;
-        Arrays.sort(citations); 
+        heapSort(citations); 
         int s = 0;
         int h = 0; 
         for (int i = 0; i < n; i++) {
@@ -13,5 +13,37 @@ class Solution {
         }
 
         return h;
+
+       
+    }
+    public static void heapify(int arr[], int n, int i) { //n current size of the heap
+    	int largest = i;
+    	int l = 2*i+1;
+    	int r=2*i+2;
+    	if(l<n && arr[l]>arr[largest]) {
+    		largest=l;
+    	}
+    	if(r<n && arr[r]>arr[largest]) {
+    		largest=r;
+    	}
+    	if( largest!=i ) {
+    		int temp=arr[i];
+    		arr[i]=arr[largest];
+    		arr[largest]=temp;
+    		heapify(arr,n,largest);
+    	}
+    	
+    }
+    public static void heapSort(int[] arr) {
+       int n=arr.length;
+       for(int i=n/2-1;i>=0;i--) {
+    	   heapify(arr,n,i);
+       }
+       for(int i=n-1;i>=0;i--) {
+    	   int temp=arr[0];
+    	   arr[0]=arr[i];
+    	   arr[i]=temp;
+    	   heapify(arr,i,0);
+       }
     }
 }
