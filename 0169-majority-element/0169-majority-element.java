a86b7445c24e571b42a25e32b153c0f322d51a38
Asmaa Mestaoui <asmaamestaoui11@gmail.com>
2025-01-28 13:21:24 +0100

    Time: 2 ms (75.67%), Space: 53.2 MB (48.91%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -2,27 +2,17 @@ import java.util.HashMap;
 
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> countMap = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (countMap.containsKey(nums[i])) {
-                countMap.put(nums[i], countMap.get(nums[i]) + 1);
-            } else {
-                countMap.put(nums[i], 1);
+       int count=0;
+       int majority=nums[0];
+       for(int i=1; i<nums.length;i++){
+            if(nums[i]!=majority && count!=0){
+                count--;
+            }else if(nums[i]!=majority && count==0){
+                majority=nums[i];
+            }else{
+                count++;
             }
-        }
-        return findKeyWithMaxValue(countMap);
-    }
-
-    public static int findKeyWithMaxValue(HashMap<Integer, Integer> map) {
-        int maxKey = -1; 
-        int maxValue = Integer.MIN_VALUE;
-
-        for (int key : map.keySet()) {
-            if (map.get(key) > maxValue) { 
-                maxValue = map.get(key);
-                maxKey = key; 
-            }
-        }
-        return maxKey; 
+       }
+       return majority;
     }
 }
