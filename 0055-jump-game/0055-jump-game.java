71e7a77f1c777703c515f7e83a0ef2692c241a3c
Asmaa Mestaoui <asmaamestaoui11@gmail.com>
2025-01-28 15:45:15 +0100

    Time: 3 ms (45.83%), Space: 45.6 MB (69.78%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -3,11 +3,12 @@ class Solution {
         if(nums.length==1 ) return true;
         int reach=0;
         for(int i=0;i<nums.length;i++){
-            reach=Math.max(reach,i+nums[i]);
-            if(reach==nums.length-1){
-                return true;
+            
+            if(reach<i){
+                return false;
             }
+            reach=Math.max(reach,i+nums[i]);
         }
-        return false;
+        return true;
     }
 }
