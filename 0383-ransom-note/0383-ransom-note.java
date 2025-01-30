100fe0aea0b2124ccd84c1fe90fde9925328fbad
Asmaa Mestaoui <asmaamestaoui11@gmail.com>
2025-01-30 18:27:07 +0100

    Time: 10 ms (60.65%), Space: 45.1 MB (58.35%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -2,18 +2,17 @@ class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         HashMap<Character, Integer> ransomNoteCount = count(ransomNote);
         HashMap<Character, Integer> magazineCount = count(magazine);
-         for (char c : ransomNoteCount.keySet()) {
+        for (char c : ransomNoteCount.keySet()) {
             int x = ransomNoteCount.get(c);
             int y = magazineCount.getOrDefault(c, 0);
             if (x > y) {
-                return false; 
+                return false;
             }
         }
-        return true; 
+        return true;
     }
 
-    
-    public static HashMap<Character,Integer> count(String string){
+    public static HashMap<Character, Integer> count(String string) {
         HashMap<Character, Integer> charCount = new HashMap<>();
         for (char c : string.toCharArray()) {
             charCount.put(c, charCount.getOrDefault(c, 0) + 1);
