aa9e49249c9407f00923f1e744fff79694606497
Asmaa Mestaoui <asmaamestaoui11@gmail.com>
2025-03-28 19:50:42 +0000

    Time: 3 ms (42.67%), Space: 45.4 MB (92.67%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,13 +1,12 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int reach=0;
-        for(int i=0;i<nums.length;i++){
-            
-            if(reach<i){
-                return false;
-            }
-            reach=Math.max(reach,i+nums[i]);
-        }
-        return true;
+          int x =0;
+          for(int i=0;i<nums.length;i++){
+                if(x<i){
+                    return false;
+                }
+                x=Math.max(x,i+nums[i]);
+          }
+          return true;
     }
 }
