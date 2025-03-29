0bc6517e26373e5d71337b483d0739178651d33c
Asmaa Mestaoui <asmaamestaoui11@gmail.com>
2025-03-29 20:12:31 +0000

    Time: 2 ms (97.59%), Space: 41.7 MB (81.71%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,24 +1,23 @@
 class Solution {
     public boolean isValid(String s) {
-       char[]  ch=s.toCharArray();
-       List<Character> par = new ArrayList<>(); 
-        for (int i=0; i<ch.length;i++){
-            char c = ch[i];
-            if(c=='[' || c=='{' || c=='('){
-                par.add(c);
-            }else{
-                if(par.isEmpty()){
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
                     return false;
                 }
-                char last = par.get(par.size()-1);
-                if ((c==']' && last=='[') || (c=='}' && last=='{' ) || (c==')' && last=='(' ) ){
-                par.remove(par.size()-1);
-                }
-                else {
+                char last = stack.pop();
+                if ((c == ')' && last != '(') || 
+                    (c == '}' && last != '{') || 
+                    (c == ']' && last != '[')) {
                     return false;
                 }
             }
         }
-        return par.isEmpty();
+        
+        return stack.isEmpty();
     }
 }
