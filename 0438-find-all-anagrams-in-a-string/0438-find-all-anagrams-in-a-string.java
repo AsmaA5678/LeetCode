c370fab5890e69ce8f077a6a7fcc8a217d0359bc
Asmaa Mestaoui <asmaamestaoui11@gmail.com>
2025-04-21 13:07:22 +0100

    Time: 11 ms (68.36%), Space: 44.8 MB (83.42%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,47 +1,24 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> result = new ArrayList<>();
-        if (s.length() < p.length()) return result;
-
-        int[] pCount = new int[26];
-        int[] sCount = new int[26];
-
-        for (int i = 0; i < p.length(); i++) {
-            pCount[p.charAt(i) - 'a']++;
-            sCount[s.charAt(i) - 'a']++;
+        List<Integer> result=new ArrayList<>();
+        if(s.length()<p.length()) return result;
+        int[] pCount=new int[26];
+        int[] sCount=new int[26];
+        for(int i=0;i<p.length();i++){
+            pCount[p.charAt(i)-'a']++;
+            sCount[s.charAt(i)-'a']++;
         }
-
-        int matches = 0;
-        for (int i = 0; i < 26; i++) {
-            if (pCount[i] == sCount[i]) matches++;
+        if(Arrays.equals(pCount, sCount)){
+            result.add(0);
         }
-
-        int l = 0;
-        for (int r = p.length(); r < s.length(); r++, l++) {
-            if (matches == 26) result.add(l);
-
-            int indexIn = s.charAt(r) - 'a';
-            int indexOut = s.charAt(l) - 'a';
-
-            // Add new char
-            sCount[indexIn]++;
-            if (sCount[indexIn] == pCount[indexIn]) {
-                matches++;
-            } else if (sCount[indexIn] - 1 == pCount[indexIn]) {
-                matches--;
-            }
-
-            // Remove old char
-            sCount[indexOut]--;
-            if (sCount[indexOut] == pCount[indexOut]) {
-                matches++;
-            } else if (sCount[indexOut] + 1 == pCount[indexOut]) {
-                matches--;
+        for(int i=p.length();i<s.length();i++){
+            sCount[s.charAt(i)-'a']++;
+            sCount[s.charAt(i-p.length())-'a']--;
+            if(Arrays.equals(pCount, sCount)){
+                result.add(i-p.length()+1);
             }
         }
-
-        if (matches == 26) result.add(l); // check last window
-
         return result;
     }
+
 }
