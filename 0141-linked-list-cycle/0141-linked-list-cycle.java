874df273b020c9daf6bb7aa98b92a8d26486323a
Ama2 <asmaamestaoui11@gmail.com>
2025-11-01 15:33:41 +0100

    Time: 0 ms (100%), Space: 47 MB (7.11%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,14 +11,14 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        HashSet<ListNode> visited = new HashSet<>();
-        ListNode current=head;
-        while(current!=null){
-            if(visited.contains(current)){
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
                 return true;
             }
-            visited.add(current);
-            current=current.next;
         }
         return false;
     }
